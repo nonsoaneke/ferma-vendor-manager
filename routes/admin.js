48f8ede21d0d868211682fd8395f6ada4cd3bcb6
Chukwunonso Aneke <anekechukwunonso@8gmail.com>
2019-08-01 09:48:48 +0100

    file upload for yaml to json convert is set

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const Keycloak = require('keycloak-connect');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const { check, validationResult } = require('express-validator');
 const prepareFileData = require('../lib/dataPrep').prepareFileData;
 
 const router = express.Router();
@@ -25,12 +26,42 @@ router.get('/', (req, res, next) => {
     res.render('admin/postProject', { title: 'New Project' });
 });
 
-router.post('/', upload.single('projectSpec'), (req, res, next) => {
+const validation = [check('name').isEmpty(), check('description').isEmpty()];
+
+function searchData(data, name) {
+    data.forEach((element, index) => {
+        if(name === element) {
+            return true;
+        }
+        return false;
+    });
+}
+
+router.post('/', upload.single('projectSpec'), check('name').isEmpty(), check('description').isEmpty(), (req, res, next) => {
     const { name, description } = req.body;
     const { file } = req;
-
     console.log(file);
-    
+    const errors = validationResult(req);
+    const specLocation = path.join(__dirname, 'apis', 'api.json');
+    fs.readFile(specLocation, (err, data) => {
+        if(err) throw new Error(err);
+        const preparedData = prepareFileData(data);
+        const searchedData = searchData(data, name);
+        if(searchedData) {
+            res.send('error', { message: 'Project Already Exists'});
+        }
+        const newData = preparedData.push({ name, description, fileName: file.path });
+        fs.writeFile(specLocation, newData, );
+        res.send("Data written successfully");
+    });
+    if(!errors.isEmpty()) {
+        return res.render('error', { title: "File Upload failure" });
+    }
+    res.render('admin/postProject', { title: "File upload success" });
 });
 
+router.get('/test', (req, res, next) => {
+    const specLocation = path.join(__dirname, 'apis', 'api.json');
+    res.send(specLocation);
+})
 module.exports = router;
